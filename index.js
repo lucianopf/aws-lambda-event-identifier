2fe41f61a54cd510b6789c23cda81e243289bfe4
lucianopf <lucianopf@outlook.com>
2015-10-21 16:34:36 -0200

    Fixing .length

--- index.js
+++ index.js
@@ -7,18 +7,20 @@ exports.eventIdentifier = function(data){
       return "cloudwatch";
     }
   }
-  if(data.Records.length>0){
-    if(data.Records[0].EventSource == "aws:sns"){
-      return "sns";
-    }
-    if(data.Records[0].eventSource == "aws:dynamodb"){
-      return "dynamodb";
-    }
-    if(data.Records[0].eventSource == "aws:s3"){
-      return "s3";
-    }
-    if(data.Records[0].eventSource == "aws:kinesis"){
-      return "s3";
+  if(data.Records){
+    if(data.Records.length>0){
+      if(data.Records[0].EventSource == "aws:sns"){
+        return "sns";
+      }
+      if(data.Records[0].eventSource == "aws:dynamodb"){
+        return "dynamodb";
+      }
+      if(data.Records[0].eventSource == "aws:s3"){
+        return "s3";
+      }
+      if(data.Records[0].eventSource == "aws:kinesis"){
+        return "s3";
+      }
     }
   }
   return null;
